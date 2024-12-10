6badca6cd5ee6384caf582170c3312978decdc59
AminulMunaDev <Aimmister2@gmail.com>
2024-12-10 12:38:24 +0600

    finished

--- app/src/main/java/com/example/recyclerviewpractice/RvAdapter.kt
+++ app/src/main/java/com/example/recyclerviewpractice/RvAdapter.kt
@@ -8,30 +8,31 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RvAdapter(var dataList :ArrayList<RvModel>,val context: Context):RecyclerView.Adapter<RvAdapter.MyViewHolder>(){
-   inner class MyViewHolder(private var view : View):RecyclerView.ViewHolder(view){
-       val profile = view.findViewById<ImageView>(R.id.item_image)
-       val name = view.findViewById<TextView>(R.id.restaurant_name)
-       val address = view.findViewById<TextView>(R.id.restaurant_address)
-   }
+class RvAdapter(var dataList: ArrayList<RvModel>, val context: Context) :
+    RecyclerView.Adapter<RvAdapter.MyViewHolder>() {
+    inner class MyViewHolder(private var view: View) : RecyclerView.ViewHolder(view) {
+        val profile: ImageView = view.findViewById(R.id.item_image)
+        val name = view.findViewById<TextView>(R.id.restaurant_name)
+        val address = view.findViewById<TextView>(R.id.restaurant_address)
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
-        val myItem =LayoutInflater.from(context).inflate(R.layout.item_list,parent,false)
+        val myItem = LayoutInflater.from(context).inflate(R.layout.item_list, parent, false)
         return MyViewHolder(myItem)
     }
 
     override fun getItemCount(): Int {
-      return dataList.size
+        return dataList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val singleData =dataList[position]
 
-        holder.profile.setImageResource(singleData.foodImage)
-        holder.name.text=singleData.restaurantName
-        holder.address.text=singleData.restaurantAddress
+        holder.profile.setImageResource(dataList[position].foodImage)
+        val singleData = dataList[position]
+        holder.name.text = singleData.restaurantName
+        holder.address.text = singleData.restaurantAddress
     }
 
 
